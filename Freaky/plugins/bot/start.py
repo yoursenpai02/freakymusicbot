92a66edfb12b108bad30e97a9a77dd767fa742b7
fuk <fuk>
2023-05-01 19:04:26 +0530

    bhk

--- Freaky/plugins/bot/start.py
+++ Freaky/plugins/bot/start.py
@@ -1,68 +1,31 @@
 import asyncio
-import time
-import random 
 
 from pyrogram import filters
 from pyrogram.types import (InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 from youtubesearchpython.__future__ import VideosSearch
 
-import config
-from config import BANNED_USERS
-from config import OWNER_ID
-from strings import get_command, get_string
-from Freaky import Telegram, YouTube, app
-from Freaky.misc import SUDOERS, _boot_
-from Freaky.plugins.playlist import del_plist_msg
-from Freaky.plugins.sudoers import sudoers_list
-from Freaky.utils.database import (add_served_chat,
+from Freaky.utilities import config
+from Freaky.utilities.config import BANNED_USERS
+from Freaky.utilities.config.config import OWNER_ID
+from Freaky.utilities.strings import get_command, get_string
+from Freaky import Telegram, YouTube, bot
+from Freaky.misc import SUDOERS
+from Freaky.plugins.play.playlist import del_plist_msg
+from Freaky.plugins.sudo.sudoers import sudoers_list
+from Freaky.modules.database import (add_served_chat,
                                        add_served_user,
-                                       get_served_chats,
-                                       get_served_users,
                                        blacklisted_chats,
                                        get_assistant, get_lang,
                                        get_userss, is_on_off,
                                        is_served_private_chat)
-from Freaky.utils.decorators.language import LanguageStart
-from Freaky.utils.formatters import get_readable_time
-from Freaky.utils.inline import (help_pannel, private_panel,
-                                     start_pannel)
+from Freaky.modules.decorators.language import LanguageStart
+from Freaky.utilities.inline import (help_pannel, private_panel, private_panelx, start_pannel)
 
 loop = asyncio.get_running_loop()
 
-PH_ON = ["https://te.legra.ph/file/e37357b824b33e799ce43.mp4",
-"https://te.legra.ph/file/d9b843b151305ce80423e.mp4",
-"https://te.legra.ph/file/48a67e54eefd53858754d.mp4",
-"https://te.legra.ph/file/1efc93298684f609f242e.mp4",
-"https://te.legra.ph/file/92a6ce8dcfeb1488bddf9.mp4",
-"https://te.legra.ph/file/321253a6d4b6130c1bb6b.mp4",
-"https://te.legra.ph/file/93feb690d082905046d79.mp4",
-"https://te.legra.ph/file/ef3c365e8c0a89a5f45a0.mp4",
-"https://te.legra.ph/file/e7a619bafdb316150c6ba.mp4",
-"https://te.legra.ph/file/4d30fe0a69ce798f2116c.mp4",
-"https://te.legra.ph/file/e146a1d583a5c2a8f705c.mp4",
-"https://te.legra.ph/file/53bec2a6870a4503f282b.mp4",
-"https://te.legra.ph/file/275e5a7e49012d6627c4d.mp4",
-"https://te.legra.ph/file/e2454f93c77e4517f016a.mp4",
-"https://te.legra.ph/file/58bee071c435075e8700c.mp4",
-"https://te.legra.ph/file/e1019f75dce3db323e36a.mp4",
-"https://te.legra.ph/file/3389155926ec29f06146c.mp4",
-"https://te.legra.ph/file/ffa7f69e7f4d64e318f2a.mp4",
-"https://te.legra.ph/file/94670e91fd133fa365493.mp4",
-"https://te.legra.ph/file/7513f63a007765b42f89f.mp4",
-"https://te.legra.ph/file/a686111e3490e64eaf009.mp4",
-"https://te.legra.ph/file/8ae83126705f7471a8724.mp4"]
 
-STKR = ["CAACAgUAAxkBAAECX3VkFpDLAyTxl1Bg5cBtoNkt38xm8gACIQoAAkCj8Fdxa8YvGPC9ny8E",
-"CAACAgUAAxkBAAECX3hkFpDULnQNeJVzc9DiJUd6tX22VwACZQgAAj3k-Vfsu_WmA6PiUy8E",
-"CAACAgUAAxkBAAECX3tkFpDda2eQTIsx5IA_ia5WtaKVegACPgkAAuTa-Vd1OleMVkKXJC8E",
-"CAACAgUAAxkBAAECX35kFpDtX_HGxsV07ryGuAWTahLzKAAC5QYAAn2oGFRhYu_MJFdcoi8E",
-"CAACAgUAAxkBAAECX4FkFpD-Ql3t0WyEKkikIhj5Lu85OQACvQsAAtFrGVSfowXjyPZUdS8E",
-"CAACAgUAAxkBAAECX4RkFpEHNA8g-HYtG4lWqSJEQztrSgACYQgAAt1i-Ff86o6jaBiyrS8E",
-"CAACAgUAAxkBAAECX4dkFpEPPEjJBgvGHyiHkNnFntSBEwACYQsAApw68Veq2du-LNgCiS8E",
-"CAACAgUAAxkBAAECX4xkFpEr_W34ps64OrdIrKX0Ffn4-QACiwgAAiFzGVQu-0euwPNspS8E"]
-
-@app.on_message(
+@bot.on_message(
     filters.command(get_command("START_COMMAND"))
     & filters.private
     & ~filters.edited
@@ -75,16 +38,14 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAKIh2QWJFvkO-DPLfpoGtjpw7GGr0X9AAJdDAACPfEZVExg5qpy2VjsLwQ")
-            return await message.reply_photo(
-                       photo=config.START_IMG_URL,
-                       caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
+            return await message.reply_text(
+                _["help_1"], reply_markup=keyboard
             )
         if name[0:4] == "song":
             return await message.reply_text(_["song_2"])
         if name[0:3] == "sta":
             m = await message.reply_text(
-                f"🥱 ɢᴇᴛᴛɪɴɢ ʏᴏᴜʀ ᴩᴇʀsᴏɴᴀʟ sᴛᴀᴛs ғʀᴏᴍ {config.MUSIC_BOT_NAME} sᴇʀᴠᴇʀ."
+                "🔎 Fetching your personal stats.!"
             )
             stats = await get_userss(message.from_user.id)
             tot = len(stats)
@@ -120,7 +81,7 @@ async def start_comm(client, message: Message, _):
                     details = stats.get(vidid)
                     title = (details["title"][:35]).title()
                     if vidid == "telegram":
-                        msg += f"🔗[ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ](https://t.me/Freaky_Support) ** ᴩʟᴀʏᴇᴅ {count} ᴛɪᴍᴇs**\n\n"
+                        msg += f"🔗[Telegram Files and Audios](https://t.me/telegram) ** played {count} times**\n\n"
                     else:
                         msg += f"🔗 [{title}](https://www.youtube.com/watch?v={vidid}) ** played {count} times**\n\n"
                 msg = _["ustats_2"].format(tot, tota, limit) + msg
@@ -142,9 +103,9 @@ async def start_comm(client, message: Message, _):
             if await is_on_off(config.LOG):
                 sender_id = message.from_user.id
                 sender_name = message.from_user.first_name
-                return await app.send_message(
+                return await bot.send_message(
                     config.LOG_GROUP_ID,
-                    f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <code>sᴜᴅᴏʟɪsᴛ</code>\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀɴᴀᴍᴇ:** {sender_name}",
+                    f"{message.from_user.mention} has just started bot to check <code>SUDOLIST</code>\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
                 )
             return
         if name[0:3] == "lyr":
@@ -155,22 +116,12 @@ async def start_comm(client, message: Message, _):
                 return await Telegram.send_split_text(message, lyrics)
             else:
                 return await message.reply_text(
-                    "ғᴀɪʟᴇᴅ ᴛᴏ ɢᴇᴛ ʟʏʀɪᴄs."
+                    "Failed to get lyrics."
                 )
         if name[0:3] == "del":
             await del_plist_msg(client=client, message=message, _=_)
-        if name == "verify":
-            await message.reply_text(f"ʜᴇʏ {message.from_user.first_name},\nᴛʜᴀɴᴋs ғᴏʀ ᴠᴇʀɪғʏɪɴɢ ʏᴏᴜʀsᴇʟғ ɪɴ {config.MUSIC_BOT_NAME}, ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ɢᴏ ʙᴀᴄᴋ ᴀɴᴅ sᴛᴀʀᴛ ᴜsɪɴɢ ᴍᴇ.")
-            if await is_on_off(config.LOG):
-                sender_id = message.from_user.id
-                sender_name = message.from_user.first_name
-                return await app.send_message(
-                    config.LOG_GROUP_ID,
-                    f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ <code>ᴠᴇʀɪғʏ ʜɪᴍsᴇʟғ</code>\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀɴᴀᴍᴇ:** {sender_name}",
-                )
-            return
         if name[0:3] == "inf":
-            m = await message.reply_text("🔎")
+            m = await message.reply_text("🔎 Fetching Info!")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
@@ -186,32 +137,32 @@ async def start_comm(client, message: Message, _):
                 link = result["link"]
                 published = result["publishedTime"]
             searched_text = f"""
-😲**ᴛʀᴀᴄᴋ ɪɴғᴏʀɴᴀᴛɪᴏɴ**😲
+🔍__**Video Track Information**__
 
-📌 **ᴛɪᴛʟᴇ:** {title}
+❇️**Title:** {title}
 
-⏳ **ᴅᴜʀᴀᴛɪᴏɴ:** {duration} ᴍɪɴᴜᴛᴇs
-👀 **ᴠɪᴇᴡs:** `{views}`
-⏰ **ᴩᴜʙʟɪsʜᴇᴅ ᴏɴ:** {published}
-🎥 **ᴄʜᴀɴɴᴇʟ:** {channel}
-📎 **ᴄʜᴀɴɴᴇʟ ʟɪɴᴋ:** [ᴠɪsɪᴛ ᴄʜᴀɴɴᴇʟ]({channellink})
-🔗 **ʟɪɴᴋ:** [ᴡᴀᴛᴄʜ ᴏɴ ʏᴏᴜᴛᴜʙᴇ]({link})
+⏳**Duration:** {duration} Mins
+👀**Views:** `{views}`
+⏰**Published Time:** {published}
+🎥**Channel Name:** {channel}
+📎**Channel Link:** [Visit From Here]({channellink})
+🔗**Video Link:** [Link]({link})
 
-💖 sᴇᴀʀᴄʜ ᴩᴏᴡᴇʀᴇᴅ ʙʏ {config.MUSIC_BOT_NAME}"""
+⚡️ __Searched Powered By {config.MUSIC_BOT_NAME}__"""
             key = InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton(
-                            text="• ʏᴏᴜᴛᴜʙᴇ •", url=f"{link}"
+                            text="🎥 Watch ", url=f"{link}"
                         ),
                         InlineKeyboardButton(
-                            text="• sᴜᴩᴩᴏʀᴛ •", url="https://t.me/Freaky_Support"
+                            text="🔄 Close", callback_data="close"
                         ),
                     ],
                 ]
             )
             await m.delete()
-            await app.send_photo(
+            await bot.send_photo(
                 message.chat.id,
                 photo=thumbnail,
                 caption=searched_text,
@@ -221,48 +172,64 @@ async def start_comm(client, message: Message, _):
             if await is_on_off(config.LOG):
                 sender_id = message.from_user.id
                 sender_name = message.from_user.first_name
-                return await app.send_message(
+                return await bot.send_message(
                     config.LOG_GROUP_ID,
-                    f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <code>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</code>\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀɴᴀᴍᴇ:** {sender_name}",
+                    f"{message.from_user.mention} has just started bot to check <code>VIDEO INFORMATION</code>\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
                 )
     else:
         try:
-            await app.resolve_peer(OWNER_ID[0])
+            await bot.resolve_peer(OWNER_ID[0])
             OWNER = OWNER_ID[0]
         except:
             OWNER = None
-        out = private_panel(_, app.username, OWNER)
+        out = private_panel(_, bot.username, OWNER)
+        outx = private_panelx(_, bot.username, OWNER)
         if config.START_IMG_URL:
             try:
-                OMFOO = random.choice(PH_ON)
-                await message.reply_sticker(random.choice(STKR))
-                await message.reply_video(
-                    video=OMFOO,
-                    caption=_["start_2"].format(
+                await message.reply_photo(
+                    photo=config.START_IMG_URL,
+                    caption=_["start_8"].format(
                         config.MUSIC_BOT_NAME
                     ),
-                    reply_markup=InlineKeyboardMarkup(out),
+                    reply_markup=InlineKeyboardMarkup(outx),
                 )
             except:
                 await message.reply_text(
                     _["start_2"].format(config.MUSIC_BOT_NAME),
                     reply_markup=InlineKeyboardMarkup(out),
+                 disable_web_page_preview=True
                 )
         else:
             await message.reply_text(
                 _["start_2"].format(config.MUSIC_BOT_NAME),
                 reply_markup=InlineKeyboardMarkup(out),
+             disable_web_page_preview=True
             )
         if await is_on_off(config.LOG):
             sender_id = message.from_user.id
             sender_name = message.from_user.first_name
-            return await app.send_message(
+            if message.from_user.username:
+                user_name = f"@{message.from_user.username}"
+            else:
+                user_name = "{message.from_user.mention}"
+            return await bot.send_photo(
                 config.LOG_GROUP_ID,
-                f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ʏᴏᴜʀ ʙᴏᴛ.\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀɴᴀᴍᴇ:** {sender_name}",
-            )
+                photo=f"https://te.legra.ph/file/fc1149f435ab50e83076c.jpg",
+                caption=f"""
+**━━━━━━━━━━━━━━━━━━━**
+**💥 𝐀𝐧 𝐔𝐬𝐞𝐫 𝐇𝐚𝐬 ❥︎ 𝐉𝐮𝐬𝐭 𝐒𝐭𝐚𝐫𝐭𝐞𝐝**
+**𝐘𝐨𝐮𝐫 🌿 𝐌𝐮𝐬𝐢𝐜 🎸 𝐁𝐨𝐭 ✨ ...**
+**━━━━━━━━━━━━━━━━━━━**
+**🥀 𝐍𝐚𝐦𝐞 ›** {sender_name}
+**🌸 𝐋𝐢𝐧𝐤 : ›** {user_name}
+**🌷 𝐈𝐃ఌ︎: »** `{message.from_user.id}`
+**━━━━━━━━━━━━━━━━━━━**
+**💐 𝐓𝐡𝐞𝐬𝐞 𝐀𝐫𝐞 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧'𝐬 𝐎𝐟**
+**𝐖𝐡𝐨 🍁 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 ఌ︎ 𝐁𝐨𝐭 💞 ...**
+**━━━━━━━━━━━━━━━━━━━**""")
 
 
-@app.on_message(
+@bot.on_message(
     filters.command(get_command("START_COMMAND"))
     & filters.group
     & ~filters.edited
@@ -270,12 +237,9 @@ async def start_comm(client, message: Message, _):
 )
 @LanguageStart
 async def testbot(client, message: Message, _):
-    OMFOO = random.choice(PH_ON)
-    OWNER = OWNER_ID[0]
-    out = start_pannel(_, app.username, OWNER)
-    return await message.reply_photo(
-               photo=config.START_IMG_URL,
-               caption=_["start_1"].format(
+    out = start_pannel(_)
+    return await message.reply_text(
+        _["start_1"].format(
             message.chat.title, config.MUSIC_BOT_NAME
         ),
         reply_markup=InlineKeyboardMarkup(out),
@@ -285,39 +249,37 @@ async def testbot(client, message: Message, _):
 welcome_group = 2
 
 
-@app.on_message(filters.new_chat_members, group=welcome_group)
+@bot.on_message(filters.new_chat_members, group=welcome_group)
 async def welcome(client, message: Message):
     chat_id = message.chat.id
     if config.PRIVATE_BOT_MODE == str(True):
         if not await is_served_private_chat(message.chat.id):
             await message.reply_text(
-                "**ᴩʀɪᴠᴀᴛᴇ ᴍᴜsɪᴄ ʙᴏᴛ**\n\nᴏɴʟʏ ғᴏʀ ᴛʜᴇ ᴄʜᴀᴛs ᴀᴜᴛʜᴏʀɪsᴇᴅ ʙʏ ᴍʏ ᴏᴡɴᴇʀ, ʀᴇǫᴜᴇsᴛ ɪɴ ᴍʏ ᴏᴡɴᴇʀ's ᴩᴍ ᴛᴏ ᴀᴜᴛʜᴏʀɪsᴇ ʏᴏᴜʀ ᴄʜᴀᴛ ᴀɴᴅ ɪғ ʏᴏᴜ ᴅᴏɴ'ᴛ ᴡᴀɴᴛ ᴛᴏ ᴅᴏ sᴏ ᴛʜᴇɴ ғᴜ*ᴋ ᴏғғ ʙᴇᴄᴀᴜsᴇ ɪ'ᴍ ʟᴇᴀᴠɪɴɢ."
+                "**Private Music Bot**\n\nOnly for authorized chats from the owner. Ask my owner to allow your chat first."
             )
-            return await app.leave_chat(message.chat.id)
+            return await bot.leave_chat(message.chat.id)
     else:
         await add_served_chat(chat_id)
     for member in message.new_chat_members:
         try:
             language = await get_lang(message.chat.id)
             _ = get_string(language)
-            if member.id == app.id:
+            if member.id == bot.id:
                 chat_type = message.chat.type
                 if chat_type != "supergroup":
                     await message.reply_text(_["start_6"])
-                    return await app.leave_chat(message.chat.id)
+                    return await bot.leave_chat(message.chat.id)
                 if chat_id in await blacklisted_chats():
                     await message.reply_text(
                         _["start_7"].format(
-                            f"https://t.me/{app.username}?start=sudolist"
+                            f"https://t.me/{bot.username}?start=sudolist"
                         )
                     )
-                    return await app.leave_chat(chat_id)
+                    return await bot.leave_chat(chat_id)
                 userbot = await get_assistant(message.chat.id)
-                OWNER = OWNER_ID[0]
-                out = start_pannel(_, app.username, OWNER)
-                await message.reply_photo(
-                    photo=config.START_IMG_URL,
-                    caption=_["start_3"].format(
+                out = start_pannel(_)
+                await message.reply_text(
+                    _["start_3"].format(
                         config.MUSIC_BOT_NAME,
                         userbot.username,
                         userbot.id,
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### 🥀 𝐁𝐞𝐬𝐭 𝐀𝐝𝐯𝐚𝐧𝐜𝐞𝐝 𝐒𝐮𝐩𝐞𝐫𝐟𝐚𝐬𝐭 𝐚𝐧𝐝 𝐒𝐦𝐚𝐫𝐭 𝐕𝐂 𝐌𝐮𝐬𝐢𝐜 𝐏𝐥𝐚𝐲𝐞𝐫 𝐁𝐨𝐭 𝐒𝐨𝐮𝐫𝐜𝐞 𝐅𝐨𝐫 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐆𝐫𝐨𝐮𝐩𝐬 𝐚𝐧𝐝 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ✨...
 
-<p align="center"><a href="https://t.me/Freaky"><img src="https://te.legra.ph/file/c6e1041c6c9a12913f57a.png"></a></p>
+<p align="center"><a href="https://t.me/Freaky"><img src="https://te.legra.ph/file/fc1149f435ab50e83076c.jpg"></a></p>
 
 
 ### 𝐃𝐞𝐩𝐥𝐨𝐲 𝐎𝐧 𝐇𝐞𝐫𝐨𝐤𝐮
